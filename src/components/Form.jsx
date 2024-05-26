ab6cc456e31f0b258edcc853d348db7874c89913
Atul Chowdhury <atulchowdhury316@gmail.com>
2024-05-26 14:48:14 +0530

    added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import PhoneInput from 'react-phone-number-input';
+import 'react-phone-number-input/style.css';
 import { CountryDropdown, RegionDropdown } from 'react-country-region-selector';
 
 const Form = () => {
@@ -9,9 +11,11 @@ const Form = () => {
     username: '',
     email: '',
     password: '',
+    countryCode: '',
     phone: '',
     country: '',
     region: '',
+    city: '',
     pan: '',
     aadhar: '',
   });
@@ -28,12 +32,20 @@ const Form = () => {
     setFormData({ ...formData, [name]: value });
   };
 
+  const handlePhoneChange = (val) => {
+    setFormData({ ...formData, phone: val });
+  };
+
   const handleCountryChange = (val) => {
-    setFormData({ ...formData, country: val, region: '' });
+    setFormData({ ...formData, country: val, region: '', city: '' });
   };
 
   const handleRegionChange = (val) => {
-    setFormData({ ...formData, region: val });
+    setFormData({ ...formData, region: val, city: '' });
+  };
+  
+  const handleCityChange = (val) => {
+    setFormData({ ...formData, city: val });
   };
 
   const validate = (field, value) => {
@@ -59,14 +71,17 @@ const Form = () => {
       error =
         'Password must contain at least one uppercase, at least one lowercase, at least one number, and at least one special character';
     }
-    if (field === 'phone' && (!value || !/^\d{10}$/.test(value))) {
-      error = 'Phone number must be exactly 10 digits';
+    if (field === 'phone' && (!value || value.length < 10 || value.length > 15)) {
+      error = 'Phone number must be between 10 and 15 digits';
     }
     if (field === 'country' && !value) {
       error = 'Country is required';
     }
     if (field === 'region' && !value) {
       error = 'Region is required';
+    }
+    if (field === 'city' && !value) {
+      error = 'city is required';
     }
     if (field === 'pan' && (!value || !/[A-Z0-9]{10}/.test(value))) {
       error = 'PAN must be a combination of uppercase characters and numbers';
@@ -93,21 +108,20 @@ const Form = () => {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen p-4 bg-slate-600">
-      <form className="w-full max-w-lg p-8 shadow-lg rounded-lg bg-slate-300" onSubmit={handleSubmit}>
-        <h2 className="text-3xl font-bold mb-6 text-green-500 underline">Registration Form</h2>
+    <div className="flex items-center justify-center min-h-screen p-4 bg-slate-600">
+      <form className="w-full max-w-lg p-8 rounded-lg shadow-lg bg-slate-300" onSubmit={handleSubmit}>
+        <h2 className="mb-6 text-3xl font-bold text-green-500 underline">Registration Form</h2>
         {[
           { name: 'firstName', label: 'First Name' },
           { name: 'lastName', label: 'Last Name' },
           { name: 'username', label: 'Username' },
           { name: 'email', label: 'Email' },
-          { name: 'password', label: 'Password', type: passwordShown ? 'text' : 'password' },
-          { name: 'phone', label: 'Phone No.', placeholder: 'country code ____ number' },
-          { name: 'pan', label: 'Pan No.' },
-          { name: 'aadhar', label: 'Aadhar No.' },
-        ].map(({ name, label, type = 'text', placeholder }) => (
+          //{ name: 'password', label: 'Password', type: passwordShown ? 'text' : 'password' },
+          //{ name: 'pan', label: 'Pan No.' },
+          //{ name: 'aadhar', label: 'Aadhar No.' },
+        ].map(({ name, label, type = 'text' }) => (
           <div key={name} className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor={name}>
+            <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor={name}>
               {label}
             </label>
             <input
@@ -118,48 +132,19 @@ const Form = () => {
               onChange={handleChange}
               onBlur={handleBlur}
               onInput={(e) => {
-                if (name === 'phone' || name === 'aadhar') {
+                if (name === 'aadhar') {
                   e.target.value = e.target.value.replace(/\D/g, '');
                 }
               }}
-              placeholder={placeholder}
               className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
                 errors[name] ? 'border-red-500' : ''
               }`}
             />
-            {errors[name] && <p className="text-red-500 text-xs italic">{errors[name]}</p>}
+            {errors[name] && <p className="text-xs italic text-red-500">{errors[name]}</p>}
           </div>
         ))}
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="country">
-            Country
-          </label>
-          <CountryDropdown
-            value={formData.country}
-            onChange={(val) => handleCountryChange(val)}
-            className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
-              errors.country ? 'border-red-500' : ''
-            }`}
-          />
-          {errors.country && <p className="text-red-500 text-xs italic">{errors.country}</p>}
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="region">
-            Region
-          </label>
-          <RegionDropdown
-            country={formData.country}
-            value={formData.region}
-            onChange={(val) => handleRegionChange(val)}
-            className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
-              errors.region ? 'border-red-500' : ''
-            }`}
-            disableWhenEmpty={true}
-          />
-          {errors.region && <p className="text-red-500 text-xs italic">{errors.region}</p>}
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+          <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="password">
             Password
           </label>
           <div className="relative">
@@ -182,13 +167,87 @@ const Form = () => {
               {passwordShown ? 'Hide' : 'Show'}
             </button>
           </div>
-          {errors.password && <p className="text-red-500 text-xs italic">{errors.password}</p>}
+          {errors.password && <p className="text-xs italic text-red-500">{errors.password}</p>}
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="phone">
+            Phone No.
+          </label>
+          <div className="flex">
+            <PhoneInput
+              name="phone"
+              value={formData.phone}
+              onChange={handlePhoneChange}
+              defaultCountry="IN"
+              className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                errors.phone ? 'border-red-500' : ''
+              }`}
+            />
+          </div>
+          {errors.phone && <p className="text-xs italic text-red-500">{errors.phone}</p>}
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="country">
+            Country
+          </label>
+          <CountryDropdown
+            value={formData.country}
+            onChange={(val) => handleCountryChange(val)}
+            className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+              errors.country ? 'border-red-500' : ''
+            }`}
+          />
+          {errors.country && <p className="text-xs italic text-red-500">{errors.country}</p>}
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="region">
+            Region
+          </label>
+          <RegionDropdown
+            country={formData.country}
+            value={formData.region}
+            onChange={(val) => handleRegionChange(val)}
+            className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+              errors.region ? 'border-red-500' : ''
+            }`}
+            disableWhenEmpty={true}
+          />
+          {errors.region && <p className="text-xs italic text-red-500">{errors.region}</p>}
+        </div>
+        <div>
+        {[
+          { name: 'pan', label: 'Pan No.' },
+          { name: 'aadhar', label: 'Aadhar No.' },
+        ].map(({ name, label, type = 'text' }) => (
+          <div key={name} className="mb-4">
+            <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor={name}>
+              {label}
+            </label>
+            <input
+              type={type}
+              name={name}
+              id={name}
+              value={formData[name]}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              onInput={(e) => {
+                if (name === 'aadhar') {
+                  e.target.value = e.target.value.replace(/\D/g, '');
+                }
+              }}
+              className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                errors[name] ? 'border-red-500' : ''
+              }`}
+            />
+            {errors[name] && <p className="text-xs italic text-red-500">{errors[name]}</p>}
+          </div>
+        ))}
         </div>
         <div className="flex items-center justify-between">
           <button
             type="submit"
             disabled={Object.keys(errors).some((key) => errors[key])}
-            className="bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline disabled:opacity-50"
+            className="px-4 py-2 font-bold text-white bg-red-400 rounded hover:bg-green-500 focus:outline-none focus:shadow-outline disabled:opacity-50"
           >
             Submit
           </button>
